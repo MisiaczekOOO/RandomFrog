bb093a7a0f7c2b344d32231545ce1759df63a081
MisiaczekOOO <mosormichal711@gmail.com>
2024-08-03 23:49:25 +0200

    deployment setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./DATABASE.json"
+import "./style.scss"
+import { useState } from "react";
 
 function App() {
+
+  const [record, setRecord] = useState();
+
+  const getRandomRecord = () => {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    setRecord(data[randomIndex])
+  }
+
   return (
-    <div>
-      Chuj
-    </div>
+    <body className="body">
+      {record && (
+        <div>
+          <a href={record.url}>link to article</a>
+          <img src={record.image_url} />
+        </div>
+      )}
+      <button onClick={getRandomRecord}>random frog</button>
+    </body>
   );
 }
 
